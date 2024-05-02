7e820e4dfee3fc5da8bb25b143c7e6fca1e29e6a
Jesse Peterson <jesse.c.peterson@gmail.com>
2024-05-02 12:15:29 -0700

    fix schema change

--- storage/mysql/schema.00001.sql
+++ storage/mysql/schema.00001.sql
@@ -1,1 +1,1 @@
-ALTER TABLE dep_names ADD COLUMN tokenpki_staging_cert_pem TEXT NULL, tokenpki_staging_cert_pem TEXT NULL;
+ALTER TABLE dep_names ADD COLUMN tokenpki_staging_cert_pem TEXT NULL, ADD COLUMN tokenpki_staging_key_pem TEXT NULL;
